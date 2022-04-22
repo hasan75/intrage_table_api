cd196b6666ee7d4ad5efef81240b5d5bfe2fcfe6
hasan75 <hasanjab14@gmail.com>
2022-04-22 17:38:51 +0600

    shorting added

--- src/component/TableReact/TableReact.js
+++ src/component/TableReact/TableReact.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import BootstrapTable from 'react-bootstrap-table-next';
 
 const TableReact = () => {
   const [data, setData] = useState([]);
@@ -7,13 +8,34 @@ const TableReact = () => {
     getData();
   }, []);
   const getData = () => {
-    axios('https://jsonplaceholder.typicode.com/comments')
-      .then((res) => res.json())
-      .then((resData) => setData(resData));
+    axios('https://jsonplaceholder.typicode.com/comments').then((res) =>
+      setData(res.data)
+    );
   };
+  const columns = [
+    {
+      dataField: 'email',
+      text: 'Email',
+      sort: 'true',
+    },
+    {
+      dataField: 'postId',
+      text: 'Product Id',
+    },
+    {
+      dataField: 'name',
+      text: 'Name',
+    },
+  ];
   return (
-    <div>
-      <h1>Tabler</h1>
+    <div className='container'>
+      <BootstrapTable
+        keyField='id'
+        data={data}
+        columns={columns}
+        striped
+        hover
+      />
     </div>
   );
 };
